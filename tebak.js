425b0d5fc51f3e78d11912ad981e555315110bdf
zadhilhaq <pemimpinerasekarang@gmail.com>
2021-10-23 16:05:21 +0700

    revisi game tebak angka pada tebakangka.html serta file tebak.js

--- tebak.js
+++ tebak.js
@@ -1,192 +1,76 @@
 const btn = document.getElementById("btn")
 btn.addEventListener("click", () => {
     alert ("Game dimulai")
-    game()
+    play()
+    hasil()
 })
 let pointp1 = 0
 let pointp2 = 0
+let player1;
+let player2;
+
 function game(){
-    let player1 = prompt("Game 1"+"\n"+"Player 1: masukkan angka")
-    let player2 = prompt("Game 1"+"\n"+"Player 2: masukkan angka")
-    
-    if(player1==player2){
-        alert ("tebakan tidak boleh sama")
-        return game()
-    }
-    else if(player1>3 || player2>3){
-        alert ("tebakan tidak boleh lebih dari 3")
-        return game()
-    }
-    else if(player1<1 || player2<1){
-        alert ("tebakan tidak boleh kurang dari 1")
-        return game()
-    }
-    else{
-        if(player1==1){
-            pointp1=pointp1+1
-            alert("Poin Sementara"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
-            alert("lanjut ke game ke-2")
-            return game2()
-        }
-        else if(player2==1){
-            pointp2= pointp2+1
-            alert("Poin Sementara"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
-            alert("lanjut ke game ke-2")
-            return game2()
+    angka = Math.floor(Math.random()*3)+1
+    player1 = prompt("Player 1: masukkan angka")
+    player2 = prompt("Player 2: masukkan angka")
+    if(!/^[0-9]+$/.test(player1)||!/^[0-9]+$/.test(player2)){
+        alert("tebakan harus menggunakan angka")
+        if(confirm("Ulangi??")){
+            return game()
         }
         else{
-            alert("Poin Sementara"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
-            alert("lanjut ke game ke-2")
-            return game2()
-        }
+            throw "exit"
+        }        
     }
-}
-function game2(){
-    let player1 = prompt("Game 2"+"\n"+"Player 1: masukkan angka")
-    let player2 = prompt("Game 2"+"\n"+"Player 2: masukkan angka")
-    if(player1==player2){
-        alert ("tebakan tidak boleh sama")
-        return game2()
-    }
-    else if(player1>3 || player2>3){
-        alert ("tebakan tidak boleh lebih dari 3")
-        return game2()
-    }
-    else if(player1<1 || player2<1){
-        alert ("tebakan tidak boleh kurang dari 1")
-        return game2()
-    }
-    else{
-        if(player1==2){
-            pointp1=pointp1+1
-            alert("Poin Sementara"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
-            alert("lanjut ke game ke-3")
-            return game3()
-        }
-        else if(player2==2){
-            pointp2= pointp2+1
-            alert("Poin Sementara"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
-            alert("lanjut ke game ke-3")
-            return game3()
+    else if(angka==player1 && angka==player2){
+        alert("tebakan tidak boleh sama")
+        if(confirm("Ulangi??")){
+            return game()
         }
         else{
-            alert("Poin Sementara"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
-            alert("lanjut ke game ke-3")
-            return game3()
+            throw "exit"
         }
     }
-
-}
-function game3(){
-    let player1 = prompt("Game 3"+"\n"+"Player 1: masukkan angka")
-    let player2 = prompt("Game 3"+"\n"+"Player 2: masukkan angka")
-    if(player1==player2){
-        alert ("tebakan tidak boleh sama")
-        return game3()
+    else if(angka==player1){
+        pointp1=pointp1+1
+        alert("player 1 menang")
+        alert("nilai yang dicari:" +angka +"\n"+"player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
     }
-    else if(player1>3 || player2>3){
-        alert ("tebakan tidak boleh lebih dari 3")
-        return game3()
+    else if(angka==player2){
+        pointp2=pointp2+1
+        alert("player 2 menang")
+        alert("nilai yang dicari:" +angka +"\n"+"player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
     }
-    else if(player1<1 || player2<1){
-        alert ("tebakan tidak boleh kurang dari 1")
-        return game3()
+    else if(angka!=player1 && angka!=player2){
+        alert("tidak ada yang benar hasil seri")
+        alert("nilai yang dicari:" +angka +"\n"+"player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)        
     }
-    else{
-        if(player1==3){
-            pointp1=pointp1+1
-            alert("Poin Sementara"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
-            alert("lanjut ke game ke-4")
-            return game4()
-        }
-        else if(player2==3){
-            pointp2= pointp2+1
-            alert("Poin Sementara"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
-            alert("lanjut ke game ke-4")
-            return game4()
-        }
-        else{
-            alert("Poin Sementara"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
-            alert("lanjut ke game ke-4")
-            return game4()
-        }
-    }
-
-}
-function game4(){
-    let player1 = prompt("Game 4"+"\n"+"Player 1: masukkan angka")
-    let player2 = prompt("Game 4"+"\n"+"Player 2: masukkan angka")
-    if(player1==player2){
-        alert ("tebakan tidak boleh sama")
-        return game4()
-    }
-    else if(player1>3 || player2>3){
-        alert ("tebakan tidak boleh lebih dari 3")
-        return game4()
-    }
-    else if(player1<1 || player2<1){
-        alert ("tebakan tidak boleh kurang dari 1")
-        return game4()
-    }
-    else{
-        if(player1==2){
-            pointp1=pointp1+1
-            alert("Poin Sementara"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
-            alert("lanjut ke game ke-5")
-            return game5()
-        }
-        else if(player2==2){
-            pointp2= pointp2+1
-            alert("Poin Sementara"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
-            alert("lanjut ke game ke-5")
-            return game5()
-        }
-        else{
-            alert("Poin Sementara"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
-            alert("lanjut ke game ke-5")
-            return game5()
-        }
-    }
-
-}
-function game5(){
-    let player1 = prompt("Game 5"+"\n"+"Player 1: masukkan angka")
-    let player2 = prompt("Game 5"+"\n"+"Player 2: masukkan angka")
-    if(player1==player2){
-        alert ("tebakan tidak boleh sama")
-        return game5()
-    }
-    else if(player1>3 || player2>3){
-        alert ("tebakan tidak boleh lebih dari 3")
-        return game5()
-    }
-    else if(player1<1 || player2<1){
-        alert ("tebakan tidak boleh kurang dari 1")
-        return game5()
-    }
-    else{
-        if(player1==1){
-            pointp1=pointp1+1
-            return hasil()
-        }
-        else if(player2==1){
-            pointp2= pointp2+1
-            return hasil()
-        }
-        else{
-            return hasil()
-        }
-    }
-
 }
 function hasil(){
     if(pointp1>pointp2){
-        alert("Player 1 menang" + "Poin Akhir"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
+        alert("Player 1 menang" +"\n" + "Poin Akhir"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
     }
     else if(pointp2>pointp1){
-        alert("Player 2 menang" + "Poin Akhir"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
+        alert("Player 2 menang" +"\n" + "Poin Akhir"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
     }
     else{
-        alert("Hasil imbang" + "Poin Akhir"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
+        alert("Hasil imbang" +"\n" + "Poin Akhir"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
     }
+}
+function lanjut(){
+    if(confirm("Lanjut??")){
+        return game()
+    }
+    else{
+        throw "exit"
+    }
+}
+function play(){
+let i=1
+do {
+    alert("Ronde ke-" + i)
+    game(i)
+    i++
+}
+while(i<6)
 }
