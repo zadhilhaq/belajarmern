821d390f685c66368de023512553606341483614
zadhilhaq <pemimpinerasekarang@gmail.com>
2021-10-24 05:19:29 +0700

    revisi kedua game tebak angka pada tebakangka.html serta file tebak.js

--- tebak.js
+++ tebak.js
@@ -8,11 +8,12 @@ let pointp1 = 0
 let pointp2 = 0
 let player1;
 let player2;
-
+// Function untuk alur game
 function game(){
     angka = Math.floor(Math.random()*3)+1
     player1 = prompt("Player 1: masukkan angka")
     player2 = prompt("Player 2: masukkan angka")
+    // validasi angka
     if(!/^[0-9]+$/.test(player1)||!/^[0-9]+$/.test(player2)){
         alert("tebakan harus menggunakan angka")
         if(confirm("Ulangi??")){
@@ -22,7 +23,8 @@ function game(){
             throw "exit"
         }        
     }
-    else if(angka==player1 && angka==player2){
+    // validasi input sama
+    else if(player1== player2){
         alert("tebakan tidak boleh sama")
         if(confirm("Ulangi??")){
             return game()
@@ -31,24 +33,48 @@ function game(){
             throw "exit"
         }
     }
+    // validasi input kurang dari
+    else if(player1<1 ||player2<1){
+        alert("tebakan tidak boleh kurang dari 1")
+        if(confirm("Ulangi??")){
+            return game()
+        }
+        else{
+            throw "exit"
+        }
+    }
+    // validasi input lebih dari 3
+    else if(player1>3 ||player2>3){
+        alert("tebakan tidak boleh lebih dari 3")
+        if(confirm("Ulangi??")){
+            return game()
+        }
+        else{
+            throw "exit"
+        }
+    }
+    //player 1 benar
     else if(angka==player1){
         pointp1=pointp1+1
         alert("player 1 menang")
         alert("nilai yang dicari:" +angka +"\n"+"player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
         lanjut()
     }
+    // player2 benar
     else if(angka==player2){
         pointp2=pointp2+1
         alert("player 2 menang")
         alert("nilai yang dicari:" +angka +"\n"+"player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
         lanjut()
     }
+    //player 1 dan player 2 salah
     else if(angka!=player1 && angka!=player2){
         alert("tidak ada yang benar hasil seri")
         alert("nilai yang dicari:" +angka +"\n"+"player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2) 
         lanjut()       
     }
 }
+// point akhir
 function hasil(){
     if(pointp1>pointp2){
         alert("Player 1 menang" +"\n" + "Poin Akhir"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
@@ -60,6 +86,7 @@ function hasil(){
         alert("Hasil imbang" +"\n" + "Poin Akhir"+ "\n" + "player 1 ="+ pointp1 + "\n" + "player 2 =" + pointp2)
     }
 }
+// konfirmasi lanjut
 function lanjut(){
     if(confirm("Lanjut??")){
         return
@@ -68,6 +95,7 @@ function lanjut(){
         throw "exit"
     }
 }
+// perulangan game sampai ke 5
 function play(){
 let i=1
 do {
