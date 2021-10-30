8d876f720607b7da40b1ebf1be4613732c1b383e
zadhilhaq <pemimpinerasekarang@gmail.com>
2021-10-30 17:14:06 +0700

    revisi mini library pada oop/index.js dan oop/index.html

--- oop/index.js
+++ oop/index.js
@@ -1,56 +1,59 @@
-//membuat class
-class Table{
-    // metode constructor harus dibuat untuk mengenisialisasi object pada class
-    constructor(init) {
-        this.init = init;
-      }
-    // membuat header tabel
-    createHeader(data) {
-        let open = "<thead><tr>";
-        let close = "</tr></thead>";
-        data.forEach((d) => {
-          open += `<th>${d}</th>`;
-        });
+class Collapse{
+  constructor(init){
+    this.init=init
+  }
+  buttonCollapse(text){
+    text= `<p style=" margin-left: 32%;">
+    <a class="btn btn-primary" data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Toggle first element</a>
+    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Toggle second element</button>
+    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">Toggle both elements</button>
+    </p>`+
+    `<div class="row">
+  <div class="col">
+    <div class="collapse multi-collapse" id="multiCollapseExample1">
+      <div class="card card-body">`+
+      this.textPertama(this.init.data)+
+        `</div>
+    </div>
+  </div>`+
+  `<div class="col">
+    <div class="collapse multi-collapse" id="multiCollapseExample2">
+      <div class="card card-body">`+
+      this.textKedua(this.init.data)+
+        `</div>
+    </div>
+  </div>`+
+  `</div>
+  `
+  return text 
+  }
+  textPertama(data){
+    let open = "<div>";
+        let close = "</div>";
+        open = `<p>${data[0]}</p>`
     
         return open + close;
-      }
-    //membuat body tabel
-    createBody(data) {
-        let open = "<tbody>";
-        let close = "</tbody>";
-    
-        data.forEach((d) => {
-          open += `
-            <tr>
-              <td>${d[0]}</td>
-              <td>${d[1]}</td>
-              <td>${d[2]}</td>
-              <td>${d[3]}</td>
-            </tr>
-          `;
-        });
+  }
+  textKedua(data){
+    let open = "<div>";
+        let close = "</div>";
+        open = `<p>${data[1]}</p>`
     
         return open + close;
-      }
-    // merender elemen (yang berisi salah satu class bootstrap) tabel ke html
-    render(element) {
-        let table =
-          "<table class='table table-bordered'>" +
-          this.createHeader(this.init.columns) +
-          this.createBody(this.init.data) +
-          "</table>";
-        element.innerHTML = table;
-      }
+  }
+  render(elemen){
+    let collapse=
+    this.buttonCollapse()
+    elemen.innerHTML= collapse
+  }
 }
-// membuat table
-const table = new Table({
-    columns: ['No','Name', 'Email', 'Educational Background'],
-    data: [
-      ['1','Reza', 'reza@example.com', 'Universitas Sumatera Utara'],
-      ['2','Dhia', 'dhia@gmail.com', 'Universitas Sumatera Utara'],
-      ['3','Ulhaq', 'ulhaq@gmail.com', 'Universitas Sumatera Utara']
-    ]
-  });
-// render app
+const isi = new Collapse({
+  data: [
+    ['JavaScript adalah bahasa pemrograman tingkat tinggi dan dinamis. JavaScript populer di internet dan dapat bekerja di sebagian besar penjelajah web populer seperti Google Chrome, Internet Explorer, Mozilla Firefox, Netscape dan Opera. Kode JavaScript dapat disisipkan dalam halaman web menggunakan tag SCRIPT.'],
+    ['Bootstrap adalah kerangka kerja CSS yang sumber terbuka dan bebas untuk merancang situs web dan aplikasi web. Kerangka kerja ini berisi templat desain berbasis HTML dan CSS untuk tipografi, formulir, tombol, navigasi, dan komponen antarmuka lainnya, serta juga ekstensi opsional JavaScript.']
+  ]
+});
+//const isi = new Collapse("init")
 const app = document.getElementById("app");
-table.render(app);
+ //isi.buttonCollapse(app);
+isi.render(app)
